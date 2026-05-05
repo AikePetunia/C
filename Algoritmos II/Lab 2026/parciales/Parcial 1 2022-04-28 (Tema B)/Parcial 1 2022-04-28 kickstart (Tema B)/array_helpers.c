bbb0d01b450999fccf31467f8280cfadd7a93171
Aike <dvhb47487963@gmail.com>
2026-05-05 08:54:18 -0300

    parcial 2022

--- Algoritmos II/Lab 2026/parciales/Parcial 1 2022-04-28 (Tema B)/Parcial 1 2022-04-28 kickstart (Tema B)/array_helpers.c
+++ Algoritmos II/Lab 2026/parciales/Parcial 1 2022-04-28 (Tema B)/Parcial 1 2022-04-28 kickstart (Tema B)/array_helpers.c
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 
 #include "array_helpers.h"
-
 /**
 * @brief returns true when reach last entry in flight table
 * @return True when is the last entre of the flight table, False otherwise
@@ -21,7 +20,7 @@ void array_dump(DeliveryTable a) {
   for (unsigned int type = 0u; type < TYPE; ++type) {
     for (unsigned int hour = 0u; hour < HOURS; ++hour) {
       Flight f = a[type][hour];
-      fprintf(stdout, "%c: flight with %u %s arrived at %u:00", f.code, f.items_amount, f.type == 0 ? "boxes" : "letters", f.hour - 1);
+      fprintf(stdout, "\n %c: flight with %u %s arrived at %u:00", f.code, f.items_amount, f.type == 0 ? "boxes" : "letters", f.hour - 1);
       if (!is_last_line(hour, type))
       {
         fprintf(stdout, "\n");
@@ -31,10 +30,32 @@ void array_dump(DeliveryTable a) {
 }
 
 unsigned int extra_space_fee_cost (DeliveryTable a) {
-  /* COMPLETAR */
-  return 0;
-}
 
+  int hour = 0;
+  int boxes_cost = 0;
+  int boxes_total = 0;
+  int letter_cost = 0;
+  int letter_total = 0;
+  while (hour < FEE_CLOSE_HOUR)  {
+    printf("a[0][hour].items_amount %d \n", a[0][hour].items_amount);
+    boxes_total += a[0][hour].items_amount;
+    if (boxes_total  > MAX_ALLOWED_BOXES){
+      printf("supero limit \n");
+      boxes_cost += BOX_PENALTY;
+    }
+
+    letter_total += a[1][hour].items_amount;
+    if (letter_total > MAX_ALLOWED_LETTERS) {
+      printf("supero limit \n");
+      letter_cost += LETTER_PENALTY;
+      }
+      hour++;
+    }
+
+  printf("box cost %d \n", boxes_cost);
+  printf("letter cost %d \n", letter_cost);
+    return boxes_cost + letter_cost;
+}
 
 void array_from_file(DeliveryTable array, const char *filepath) {
   FILE *file = NULL;
@@ -47,14 +68,40 @@ void array_from_file(DeliveryTable array, const char *filepath) {
 
   char code;
   int i = 0;
-  while (/* COMPLETAR: lectura completa de todos los datos */) {
-    int res = fscanf(/* COMPLETAR: lectura de codigo de vuelo */);
-    if (res != 1) {
+  while (!(feof(file))) {
+    int res = fscanf(file, "_%c_", &code);
+    if (res != 1)
+    {
       fprintf(stderr, "Invalid file.\n");
       exit(EXIT_FAILURE);
     }
-    /* COMPLETAR: Generar y guardar ambos Flight en el array multidimensional */
-    Flight flight_boxes = /* completar... */
-    Flight flight_letters = /* completar... */
+    
+    printf("hour %d & ", i);
+      if (i > HOURS) {
+      break;
+    } 
+    printf("code: %c \n", code);
+
+    Flight flight_boxes = flight_from_file(file, code, 0);
+    // printf("flight_boxes.code: %c \n", code);
+    // printf("flight_boxes.type: %c \n", flight_boxes.type);
+    array[flight_boxes.type][flight_boxes.hour -1] = flight_boxes;
+
+    Flight flight_letters = flight_from_file(file, code, 1);
+    //printf("flight_letters.code: %c \n", code);
+    //printf("flight_letters.type: %c \n", flight_letters.type);
+    array[flight_letters.type][flight_letters.hour -1] = flight_letters;
+
+
+      i++;
+
   }
+  // if (i ...) {}
 }
+
+
+/*
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -c array_helpers.c flight.c main.c
+gcc -Wall -Werror -Wextra -pedantic -std=c99 array_helpers.o flight.o main.o -o delivery
+./delivery ./inputs/example1230.in 
+*/
--- Algoritmos II/Lab 2026/parciales/Parcial 1 2022-04-28 (Tema B)/Parcial 1 2022-04-28 kickstart (Tema B)/flight.c
+++ Algoritmos II/Lab 2026/parciales/Parcial 1 2022-04-28 (Tema B)/Parcial 1 2022-04-28 kickstart (Tema B)/flight.c
@@ -13,7 +13,20 @@ Flight flight_from_file(FILE* file, char code, item_t type)
     flight.code = code;
     flight.type = type;
 
-    /* COMPLETAR */
-
+    printf("\n type: %d", type);
+    printf("\n flight.type: %d", flight.type);
+    
+    unsigned int hour = 0u;
+    unsigned int items_amount = 0u;
+    int res = fscanf(file, EXPECTED_FLIGHT_FILE_FORMAT, &hour, &items_amount);
+    if (res != AMOUNT_OF_FLIGHT_VARS) {
+      printf("no hay suficientes coso\n");
+      exit(EXIT_FAILURE);
+    }
+    flight.hour = hour;
+    flight.items_amount = items_amount;
+    
+        printf("hour: %d & ", hour);
+        printf("items_amout: %d \n", items_amount);
     return flight;
 }
